cf84aae9df63f9ef50f5f82e4c699420964dba1e
patel9619ali <patel9619ali@gmail.com>
2026-02-20 14:08:42 +0530

    CLAUDNARY ADDED

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,20 +1,31 @@
-export default {
+export default ({ env }) => ({
   email: {
     config: {
       provider: "@strapi/provider-email-nodemailer",
       providerOptions: {
-        host: process.env.SMTP_HOST || "smtp.gmail.com",
-        port: process.env.SMTP_PORT || 465,
-        secure: true, // true for 465, false for other ports
+        host: env("SMTP_HOST", "smtp.gmail.com"),
+        port: env.int("SMTP_PORT", 465),
+        secure: true,
         auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS,
+          user: env("SMTP_USER"),
+          pass: env("SMTP_PASS"),
         },
-      },
-      settings: {
-        defaultFrom: process.env.SMTP_USER || "startup.by1234@gmail.com",
-        defaultReplyTo: "sameeran7608@gmail.com",
       },
     },
   },
-}
+
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
